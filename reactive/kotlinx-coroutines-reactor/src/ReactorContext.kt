f9af4a187129442a1d1470860ccb774186ff44d7
sokolova <marysok17@gmail.com>
2019-06-20 17:50:27 +0300

    reactorPublish() not exposed, PublisherCoroutine is marked as InternalCoroutinesApi

--- reactive/kotlinx-coroutines-reactor/src/ReactorContext.kt
+++ reactive/kotlinx-coroutines-reactor/src/ReactorContext.kt
@@ -1,11 +1,47 @@
 package kotlinx.coroutines.reactor
 
 import reactor.util.context.Context
-import kotlin.coroutines.AbstractCoroutineContextElement
-import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.*
 
-class ReactorContext(val context: Context) : AbstractCoroutineContextElement(ReactorContext) {
+/**
+ * Marks coroutine context element that contains Reactor's [Context] elements in [context] for seamless integration
+ * between [CoroutineContext] and Reactor's [Context].
+ *
+ * [Context.asCoroutineContext] is defined to add Reactor's [Context] elements as part of [CoroutineContext].
+ *
+ * Reactor builders: [mono], [flux] can extract the reactor context from their coroutine context and
+ * pass it on. Modifications of reactor context can be retrieved by ```coroutineContext[ReactorContext]```.
+ *
+ * Example usage:
+ *
+ * Passing reactor context from coroutine builder to reactor entity:
+ *
+ * ```
+ * launch(Context.of(1, "1").asCoroutineContext()) {
+ *   mono {
+ *     assertEquals(coroutineContext[ReactorContext]!!.context.get(1), "1")
+ *   }.subscribe()
+ * }
+ * ```
+ *
+ * Accessing modified reactor context enriched from downstream via coroutine context:
+ *
+ * ```
+ * launch {
+ *   mono {
+ *     assertEquals(coroutineContext[ReactorContext]!!.context.get(2), "2")
+ *   }.subscriberContext(Context.of(2, "2"))
+ *    .subscribe()
+ * }
+ * ```
+ */
+public class ReactorContext(val context: Context) : AbstractCoroutineContextElement(ReactorContext) {
     companion object Key : CoroutineContext.Key<ReactorContext>
 }
 
-fun Context.asCoroutineContext(): CoroutineContext = ReactorContext(this)
+
+/**
+ * Wraps [Context] into [ReactorContext] so that Reactor's [Context] elements
+ * could be added as part of [CoroutineContext] and retrieved via ```coroutineContext[ReactorContext]```.
+ */
+public fun Context.asCoroutineContext(): CoroutineContext = ReactorContext(this)
--- reactive/kotlinx-coroutines-reactor/test/ReactorContextTest.kt
+++ reactive/kotlinx-coroutines-reactor/test/ReactorContextTest.kt
@@ -1,14 +1,14 @@
 package kotlinx.coroutines.reactor
 
-import kotlinx.coroutines.reactive.awaitFirst
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.reactive.*
 import org.junit.Test
 import reactor.util.context.Context
 import kotlin.test.assertEquals
 
 class ReactorContextTest {
     @Test
-    fun monoHookedContext() = runBlocking(Context.of(1, "1", 7, "7").asCoroutineContext()) {
+    fun testMonoHookedContext() = runBlocking(Context.of(1, "1", 7, "7").asCoroutineContext()) {
         val mono = mono {
             val ctx = coroutineContext[ReactorContext]?.context
             buildString {
@@ -20,7 +20,7 @@ class ReactorContextTest {
     }
 
     @Test
-    fun fluxTest() = runBlocking<Unit>(Context.of(1, "1", 7, "7").asCoroutineContext()) {
+    fun testFluxContext() = runBlocking<Unit>(Context.of(1, "1", 7, "7").asCoroutineContext()) {
         val flux = flux<String?> {
             val ctx = coroutineContext[ReactorContext]!!.context
             (1 .. 7).forEach { send(ctx.getOrDefault(it, "noValue")) }
@@ -28,5 +28,29 @@ class ReactorContextTest {
             .subscriberContext { ctx -> ctx.put(6, "6") }
         var i = 0
         flux.subscribe { str -> i++; assertEquals(str, i.toString()) }
+    }
+
+    @Test
+    fun testContextFromCoroutineBuilderToReactor() = runBlocking {
+        launch(Context.of(1, "1").asCoroutineContext()) {
+            val mono = mono {
+                assertEquals(coroutineContext[ReactorContext]!!.context[1], "1")
+            }
+            mono.subscribe()
+        }
+        println()
+    }
+
+    @Test
+    fun testContextFromReactorToCoroutine() = runBlocking(Context.of(1, "global 1").asCoroutineContext()) {
+        mono {
+            assertEquals(coroutineContext[ReactorContext]!!.context[2], "2")
+            assertEquals(coroutineContext[ReactorContext]!!.context[1], "global 1")
+        }.subscriberContext(Context.of(2, "2", 3, "3"))
+                .subscribe()
+        mono {
+            assertEquals(coroutineContext[ReactorContext]!!.context.getOrDefault(1, "no value"), "global 1")
+        }.subscribe()
+        println()
     }
 }
